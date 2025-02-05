71e9a48cd0831522a0537a60ef2b4bbaf8adcc58
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2025-02-05 16:09:19 +0000

    Create context processor for dynamic current year

---
+++ home/context_processors.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+
+def current_year(request):
+    # Get the current year
+    return {
+        'current_year': datetime.now().year
+    }
+    
--- ogochuks/settings.py
+++ ogochuks/settings.py
@@ -121,6 +121,7 @@ TEMPLATES = [
                     'django.contrib.messages.context_processors.messages',
                     'django.template.context_processors.media',
                     'cart.contexts.cart_contents'
+                    'home.context_processors.current_year',
                 ],
                 'builtins': [
                     'crispy_forms.templatetags.crispy_forms_tags',
